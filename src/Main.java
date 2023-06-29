2518fdd47d49fd2b6168356d6198fcc737f27690
itily <ilyha_roza@mail.ru>
2023-06-29 08:45:59 +0300

    HW 4

--- src/Main.java
+++ src/Main.java
@@ -6,7 +6,7 @@ public class Main {
     public static void main(String[] args) {
         Solution solution = new Solution();
 
-//        revertList(solution);
+        revertList(solution);
         checkIsValidParentheses(solution);
     }
 
