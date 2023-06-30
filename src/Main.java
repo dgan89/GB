3eb2fcd32359ce64d9def3cdd0f2a096ce167953
itily <ilyha_roza@mail.ru>
2023-06-30 20:00:35 +0300

    HW 5

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,43 @@
+import java.util.Arrays;
+import java.util.List;
+
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Solution solution = new Solution();
 
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
+        searchWord(solution);
+        searchDuplicates(solution);
+    }
 
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
-        }
+    /**
+     * Поиск дубликатов
+     *
+     * @param solution
+     */
+    private static void searchDuplicates(Solution solution) {
+        List<String> items = Arrays.asList(
+                "Иван Иванов",
+                "Светлана Петрова",
+                "Анна Мусина",
+                "Анна Крутова",
+                "Иван Ежов"
+        );
+        List<String> result = solution.searchDuplicates(items);
+        System.out.println(result);
+    }
+
+    /**
+     * Поиск колличества лов в строке
+     *
+     * @param solution
+     */
+    private static void searchWord(Solution solution) {
+        String income = "Россия идет вперед всей планеты. Планета — это не Россия.";
+        String search = "Россия";
+
+        Integer count = solution.searchCount(income, search);
+        System.out.printf("%s - %d", search, count);
     }
 }
